74d06c0b37a0d873b91b601aeb5af10a7935da22
KurtBliss <kurtbliss3@gmail.com>
2021-11-12 12:18:15 -0500

    update

--- TimeManager/Control.gd
+++ TimeManager/Control.gd
@@ -1,15 +1,9 @@
 extends Node
-
 onready var textEdit = $TextEdit
-onready var popupMenu = $PopupMenu
-onready var colorPicker = $ColorPicker
 onready var timerWindow = $WindowDialog
-
 var rmb = false
-
 var following = false
 var dragging_start_position = Vector2()
-
 enum {TIMER_PREFIX, TIMER_ENDFIX, TIME_LEFT, TIMER_NODE}
 var timers = []
 var timer_template = {
@@ -17,43 +11,26 @@ var timer_template = {
 	TIMER_ENDFIX: "]",
 	TIMER_NODE: null
 }
+const savelocation = "savegame.save"
 
 func _ready():
 	OS.set_borderless_window(true)
 	textEdit.grab_focus()
 	textEdit.cursor_set_column(3)
+	load_game()
 
 func _process(delta):
 	if Input.is_action_pressed("shift"):
 		if Input.is_action_just_pressed("enter"):
 			process_line_command()
-	
 	if Input.is_action_just_pressed("alt"):
-#		if popupMenu.visible:
-#			popupMenu.visible = false
-#		else:
-#			$PopupMenu.popup_centered()
 		timerWindow.popup_centered()
-	
-#	if Input.is_action_just_pressed("borderless"):
-##		if OS.is_window_focused():
-##			print("Focus")
-#			if OS.get_borderless_window():
-#				OS.set_borderless_window(false)
-#				pass
-##		else: 
-##			print("Not Focus")
-#			elif !OS.get_borderless_window():
-#				OS.set_borderless_window(true)
-
+	OS.set_borderless_window(true)
 	if following:
 		OS.set_window_position(OS.window_position + textEdit.get_global_mouse_position() - dragging_start_position)
-	
 	var _min = floor($Timer.time_left / 60)
 	var _sec = floor($Timer.time_left - (_min * 60))
-	
 	$WindowDialog/CenterContainer/VBoxContainer/HBoxContainer/Label2.text = str(_min) + " : " + str(_sec)
-	
 	$label_timer.text = $WindowDialog/CenterContainer/VBoxContainer/HBoxContainer/Label2.text
 	
 	if Input.is_action_pressed("cmd"):
@@ -84,17 +61,7 @@ func check():
 	textEdit.set_line(textEdit.cursor_get_line(), line_content)
 	textEdit.update()
 	textEdit.cursor_set_column(textEdit.cursor_get_column()+offset)
-#
-#func find():
-#	var line = textEdit.cursor_get_line()
-#	var line_content =  textEdit.get_line(line)
-#	var var_find 
-#	var lines = textEdit.get_line_count()
-#	textEdit.update()
-#	textEdit.cursor_set_line(var_find)	
-#	textEdit.cursor_set_column(line_content.length())
 
-	
 	
 func process_line_command():
 	var prev_line_number = textEdit.cursor_get_line()-1
@@ -103,17 +70,6 @@ func process_line_command():
 	textEdit.cursor_set_line(prev_line_number)
 	textEdit.cursor_set_column(prev_line_length)
 
-func _on_ColorPicker_color_changed(color):
-	colorPicker.visible = false
-	pass # Replace with function body.
-
-
-func _on_PopupMenu_id_pressed(id):
-	if id == 1: #color pick
-		colorPicker.visible = true
-	if id == 0: #
-		timerWindow.popup_centered()
-		pass
 
 func _on_LineEdit_text_entered(new_text):
 	$Timer.start(float(new_text)*60)
@@ -125,23 +81,6 @@ func _on_Timer_timeout():
 
 func _on_Button_pressed():
 	_on_LineEdit_text_entered($WindowDialog/CenterContainer/VBoxContainer/LineEdit.text)
-	
-
-#
-#	if Input.is_action_just_pressed("enter"):
-#		var txt = $TextEdit.get_line($TextEdit.cursor_get_line()-1)
-#		if txt == "cls":
-#			$TextEdit.text = ""
-
-
-#	if Input.is_action_just_pressed("cmd"):
-#		$TextEdit.set_line(0, "yes")
-#		$TextEdit.update()
-#	if Input.is_action_just_released("cmd"):
-#		$TextEdit.set_line(0, "no")
-#		$TextEdit.update()
-#
-#
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -159,33 +98,33 @@ func _input(event):
 				a.x -= 5
 				a.y -= 5
 				OS.set_window_size(a)
-				print("DOWN")
 	if event is InputEventKey:
 		if event.scancode == KEY_F1 and event.pressed:
 			OS.set_borderless_window(!OS.get_borderless_window())
 
 
-#
-#
-#func _on_TextEdit_gui_input(event):
-#	if event is InputEventMouseButton:
-#		if event.get_button_index() == 2:
-#			following = !following
-#			dragging_start_position = textEdit.get_local_mouse_position()
-#		if rmb:
-#			if event.get_button_index() == BUTTON_WHEEL_UP:
-#				var a = OS.get_window_size()
-#				a.x += 5
-#				a.y += 5
-#				OS.set_window_size(a)
-#			if event.get_button_index() == BUTTON_WHEEL_DOWN:
-#				var a = OS.get_window_size()
-#				a.x -= 5
-#				a.y -= 5
-#				OS.set_window_size(a)
-#				print("DOWN")
-#	if event is InputEventKey:
-#		if event.scancode == KEY_F1 and event.pressed:
-#			OS.set_borderless_window(!OS.get_borderless_window())
-#
-#
+func save():
+	var save_game = File.new()
+	save_game.open(savelocation, File.WRITE)
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for i in save_nodes:
+		var node_data = i.call("save");
+		save_game.store_line(to_json(node_data))
+	save_game.store_string(textEdit.text)
+	save_game.close()
+
+# Note: This can be called from anywhere inside the tree. This function
+# is path independent.
+func load_game():
+	var save_game = File.new()
+	if not save_game.file_exists(savelocation):
+		return # Error! We don't have a save to load.
+	save_game.open(savelocation, File.READ)
+	textEdit.text = save_game.get_as_text()
+	save_game.close()
+
+func _on_Control_tree_exiting():
+	save()
+	pass # Replace with function body.
+
+
